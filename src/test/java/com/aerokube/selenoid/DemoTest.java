29d4b9c5f95200398c6fd623d5450f71eaae9b96
Ivan Krutov <vania-pooh@vania-pooh.com>
2018-02-25 16:51:05 +0300

    Removed useless stuff from the test

--- src/test/java/com/aerokube/selenoid/DemoTest.java
+++ src/test/java/com/aerokube/selenoid/DemoTest.java
@@ -20,22 +20,17 @@ public class DemoTest {
     @Before
     public void openDriver() throws Exception {
         final DesiredCapabilities browser = DesiredCapabilities.chrome();
-        browser.setCapability("enableVNC", true);
-        browser.setCapability("screenResolution", "1920x1080x24");
         driver = new RemoteWebDriver(new URL(
-                "http://test:test-password@95.85.11.105:4444/wd/hub"
+                "http://selenoid.example.com:4444/wd/hub" //Replace with correct host and port
         ), browser);
-        driver.manage().window().setSize(new Dimension(1920, 1080));
     }
 
     @Test
     public void browserTest() throws Exception {
         try {
-            Thread.sleep(3000);
             driver.get("http://duckduckgo.com/");
             WebElement input = driver.findElement(By.cssSelector("input#search_form_input_homepage"));
             input.sendKeys(Keys.chord("selenium", Keys.ENTER));
-            Thread.sleep(10000);
         } finally {
             takeScreenshot(driver);
         }
