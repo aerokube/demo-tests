f9972820a0bd322c3b5cc300252d6c06a4f201a5
Ivan Krutov <vania-pooh@vania-pooh.com>
2019-02-13 18:13:00 +0300

    Added Android remote apk test

---
+++ src/test/java/com/aerokube/selenoid/AndroidRemoteApkTest.java
@@ -0,0 +1,52 @@
+package com.aerokube.selenoid;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+import static com.aerokube.selenoid.DemoTest.takeScreenshot;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class AndroidRemoteApkTest {
+
+    private RemoteWebDriver driver;
+
+    @Before
+    public void openDriver() throws Exception {
+        final DesiredCapabilities device = new DesiredCapabilities();
+        device.setCapability("browserName", "android");
+        device.setCapability("version", "7.1");
+        device.setCapability("app", "http://ci.example.com/game2048.apk"); //APK from https://www.apkmirror.com/apk/androbaby/2048/
+        device.setCapability("appPackage", "com.androbaby.game2048");
+        device.setCapability("appActivity", "com.androbaby.game2048.MainActivity");
+        device.setCapability("enableVNC", true);
+//        device.setCapability("enableVideo", true); // Uncomment this to record video
+
+        driver = new RemoteWebDriver(new URL(
+                "http://selenium.example.com:4444/wd/hub" //Replace with correct host and port
+        ), device);
+    }
+
+    @Test
+    public void browserTest() throws Exception {
+        try {
+            driver.findElement(By.xpath("//*[@text=\"Start Game\"]")).click();
+        } finally {
+            takeScreenshot(driver);
+        }
+    }
+
+    @After
+    public void closeDriver(){
+        if (driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+}
